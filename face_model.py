10fe786205b98b7c3fc334e41a98dc506632fb2d
shivam verma <shivamv2131@gmail.com>
2026-06-18 17:04:39 +0530

    face model changed

--- face_model.py
+++ face_model.py
@@ -1,4 +1,4 @@
 from insightface.app import FaceAnalysis
 
-face_app = FaceAnalysis(name="buffalo_s",allowed_modules=["detection", "recognition"])
+face_app = FaceAnalysis(name="buffalo_l",allowed_modules=["detection", "recognition"])
 face_app.prepare(ctx_id=-1)
