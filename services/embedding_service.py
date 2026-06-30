2ef62dbba1ffaf2b445c99f14a59e3d43ece9fa6
shivam verma <shivamv2131@gmail.com>
2026-06-30 22:09:42 +0530

    opti1 success

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import requests
-import time
 
 from face_model import face_app
 
@@ -19,23 +18,23 @@ def url_to_image(url):
 def generate_embeddings(photos):
     face_embeddings = []
 
-    overall_start = time.time()
+    
 
     for photo in photos:
-        photo_start = time.time()
+        
         photo_id = photo["photo_id"]
         url = photo["url"]
 
-        download_start = time.time()
+        
         img = url_to_image(url)
-        print(f"Download + Decode: {time.time() - download_start:.2f}s")
+        
 
         if img is None:
             continue
 
-        embedding_start = time.time()    
+            
         faces = face_app.get(img)
-        print(f"Face Detection + Embedding: {time.time() - embedding_start:.2f}s")
+        
 
         for i, face in enumerate(faces):
             face_embeddings.append({
@@ -43,9 +42,9 @@ def generate_embeddings(photos):
                 "face_index": i,
                 "embedding": face.embedding.tolist()
             })
-        print(f"Total for photo: {time.time() - photo_start:.2f}s")
-        print("-" * 40)    
+        
+            
 
-    print(f"TOTAL TIME: {time.time() - overall_start:.2f}s")    
+        
 
     return face_embeddings
